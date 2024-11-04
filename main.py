c895110460e0c00f58b2a3fb15b2a7ad9107c71d
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2024-11-04 17:51:47 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,77 @@
+
+def reverse(text: str) -> str:
+    return text[::-1]
+
+def first_letter_big(text: str) -> str:
+    return text[0].upper() + text[1:].lower()
+
+def bubblesort(l: list) -> list:
+    n: int = len(l)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+    return l
+
+def linearSearch(l: list, c: int) -> int:
+    for i in range(len(l)):
+        if l[i] == c:
+            return i
+    return -1
+
+def binarySearch(l: list, c: int) -> int:
+    n: int = len(l)
+    L: int = 0
+    R: int = n - 1
+    
+    while L < R:
+        M: int = (L + R) // 2
+        if l[M] == c:
+            return M
+        elif l[M] < c:
+            L: int = M
+        elif l[M] > c:
+            R: int = M
+    return -1
+
+def maxHeap(l: list, n: int, i: int) -> list:
+    largest: int = i
+    left: int = i*2+1
+    right: int = i*2+2
+    
+    if left < n and l[left] > l[largest]:
+        largest: int = left
+    if right < n and l[right] > l[largest]:
+        largest: int = right
+    if largest != i:
+        l[largest], l[i] = l[i], l[largest]
+        l: list = maxHeap(l, n, largest)
+    return l
+
+def heapSort(l: list) -> list:
+    n: int = len(l)
+    for i in range(n // 2 - 1, -1, -1):
+        l: list = maxHeap(l, n, i)
+    for i in range(n-1, 0, -1):
+        l[0], l[i] = l[i], l[0]
+        maxHeap(l, i, 0)
+    return l
+
+def check(l: list) -> bool:
+    for i in range(len(l)-1):
+        if l[i] > l[i+1]:
+            return False
+    return True
+
+def heapSortOptimized(l: list) -> list:
+    n: int = len(l)
+    L: int = 0
+    R: int = n - 1
+    while L < R:
+        if l[L] > l[R]:
+            l[L], l[R] = l[R], l[L]
+        L += 1
+        R -= 1
+    if check(l):
+        return l
+    return heapSort(l)
---
+++ test_main.py
@@ -0,0 +1,75 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_reverse(self):
+        text: str = "Hallo"
+        revers: str = "ollaH"
+        self.assertEqual(main.reverse(text), revers)
+    
+    def test_first_letter_big(self):
+        text: str = "mARK"
+        first_big: str = "Mark"
+        self.assertEqual(main.first_letter_big(text), first_big)
+    
+    def test_bubblesort(self):
+        l: list = [5,4,8,6]
+        sort: list = [4,5,6,8]
+        self.assertEqual(main.bubblesort(l), sort)
+        
+        l: list = [2,2,1,5,8,9]
+        sort: list = [1,2,2,5,8,9]
+        self.assertEqual(main.bubblesort(l), sort)
+        
+        l: list = [-1,-1,-2,5,6]
+        sort: list = [-2,-1,-1,5,6]
+        self.assertEqual(main.bubblesort(l), sort)
+    
+    def test_linearSearch(self):
+        l: list = [1,2,3,4,5,6,7,8,9,10]
+        self.assertEqual(main.linearSearch(l, 3), 2)
+        
+        l: list = [1,2,3,3,4,5,6,7,8,9,10]
+        self.assertEqual(main.linearSearch(l, 3), 2)
+        
+    def test_binarySearch(self):
+        l: list = [1,2,3,4,5,6,7,8,9,10]
+        self.assertEqual(main.binarySearch(l, 3), 2)
+        
+        l: list = [1,2,3,3,4,5,6,7,8,9,10]
+        self.assertEqual(l[main.binarySearch(l, 3)], 3)
+        
+    def test_heapSort(self):
+        l: list = [1,2,3,4,5,6,7,8,9,10]
+        self.assertEqual(main.heapSort(l), l)
+        
+        l: list = [10,9,8,7,6,5,4,3,2,1]
+        self.assertEqual(main.heapSort(l), [1,2,3,4,5,6,7,8,9,10])
+        
+        l: list = [4,5,8,7,1,8,-1,-5,-3]
+        self.assertEqual(main.heapSort(l), [-5,-3,-1,1,4,5,7,8,8])
+        
+        l: list = [1,2,3,4,5,6,6,6,7,7,8,9,10]
+        self.assertEqual(main.heapSort(l), l)
+
+    def test_check(self):
+        l: list = [1,2,3,4,5]
+        self.assertTrue(main.check(l))
+        l: list = [5,4,3,2,1]
+        self.assertFalse(main.check(l))
+        
+    def test_heapSortOptimized(self):
+        l: list = [1,2,3,4,5,6,7,8,9,10]
+        self.assertEqual(main.heapSort(l), l)
+        
+        l: list = [10,9,8,7,6,5,4,3,2,1]
+        self.assertEqual(main.heapSort(l), [1,2,3,4,5,6,7,8,9,10])
+        
+        l: list = [4,5,8,7,1,8,-1,-5,-3]
+        self.assertEqual(main.heapSort(l), [-5,-3,-1,1,4,5,7,8,8])
+        
+        l: list = [1,2,3,4,5,6,6,6,7,7,8,9,10]
+        self.assertEqual(main.heapSort(l), l)
+    
+if __name__ == "__main__":
+    unittest.main()
